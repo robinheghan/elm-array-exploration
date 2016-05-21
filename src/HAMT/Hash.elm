c13a43890e12de214fb54d3c449af40c31d85632
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2016-05-21 02:04:40 +0200

    Ported murmur 3 to elm

--- src/HAMT/Hash.elm
+++ src/HAMT/Hash.elm
@@ -1,8 +1,107 @@
-module HAMT.Hash exposing (hash)
+module HAMT.Hash exposing (string)
 
-import HAMT.Native.Hash
+import Bitwise exposing (..)
+import String
+import Char
 
 
-hash : a -> Int
-hash obj =
-    Native.Hash.hash obj
+string : Int -> String -> Int
+string seed str =
+    let
+        codes =
+            str
+                |> String.toList
+                |> List.map Char.toCode
+    in
+        seed
+            |> hashChars codes
+            |> finalize (String.length str)
+
+
+hashChars : List Int -> Int -> Int
+hashChars codes acc =
+    if List.isEmpty codes then
+        acc
+    else
+        let
+            firstFour =
+                List.take 4 codes
+
+            rest =
+                List.drop 4 codes
+
+            hash =
+                hashFourChars firstFour 0
+                    |> mix acc
+        in
+            -- Only step if we hashed a full set of characters
+            if List.length firstFour == 4 then
+                hashChars rest (step hash)
+            else
+                hashChars rest hash
+
+
+hashFourChars : List Int -> Int -> Int
+hashFourChars codes shift =
+    case codes of
+        x :: xs ->
+            ((x `and` 0xFF) `shiftLeft` shift) `or` hashFourChars xs (shift + 8)
+
+        _ ->
+            0
+
+
+mix : Int -> Int -> Int
+mix h1 h2 =
+    let
+        k1 =
+            mur 0xCC9E2D51 h2
+
+        k2 =
+            (k1 `shiftLeft` 15) `or` (k1 `shiftRightLogical` 17)
+
+        k3 =
+            mur 0x1B873593 k2
+    in
+        h1 `Bitwise.xor` k3
+
+
+mur : Int -> Int -> Int
+mur c h =
+    (((h `and` 0xFFFF) * c) + ((((h `shiftRightLogical` 16) * c) `and` 0xFFFF) `shiftLeft` 16)) `and` 0xFFFFFFFF
+
+
+step : Int -> Int
+step acc =
+    let
+        h1 =
+            (acc `shiftLeft` 13) `or` (acc `shiftRightLogical` 19)
+
+        h1b =
+            mur 5 h1
+    in
+        ((h1b `and` 0xFFFF) + 0x6B64) + ((((h1b `shiftRightLogical` 16) + 0xE654) `and` 0xFFFF) `shiftLeft` 16)
+
+
+finalize : Int -> Int -> Int
+finalize strLength acc =
+    let
+        h1 =
+            (acc `Bitwise.xor` strLength)
+
+        h2 =
+            h1 `Bitwise.xor` (h1 `shiftRightLogical` 16)
+
+        h3 =
+            mur 0x85EBCA6B h2
+
+        h4 =
+            h3 `Bitwise.xor` (h3 `shiftRightLogical` 13)
+
+        h5 =
+            mur 0xC2B2AE35 h4
+
+        h6 =
+            h5 `Bitwise.xor` (h5 `shiftRightLogical` 16)
+    in
+        h6 `shiftRightLogical` 0
--- test/Test/NodeList.elm
+++ test/Test/NodeList.elm
@@ -1,6 +1,7 @@
 module Test.NodeList exposing (tests)
 
 import HAMT.NodeList exposing (..)
+import HAMT.Hash as Hash
 import ElmTest exposing (..)
 
 
@@ -13,6 +14,7 @@ tests : Test
 tests =
     suite "NodeList tests"
         [ hashPath
+        , hashing
         ]
 
 
@@ -25,4 +27,21 @@ hashPath =
         , test "fourth level" <| assertEqual 27 <| hashPositionAtDepth 3 hash
         , test "fifth level" <| assertEqual 23 <| hashPositionAtDepth 4 hash
         , test "sixth level" <| assertEqual 26 <| hashPositionAtDepth 5 hash
+        ]
+
+
+hashFn : String -> Int
+hashFn =
+    Hash.string 63476
+
+
+hashing : Test
+hashing =
+    suite "Hashing"
+        [ test "int" <| assertEqual 1992578978 <| hashFn <| toString -102433675
+        , test "float" <| assertEqual 335970363 <| hashFn <| toString 4.32
+        , test "rec" <| assertEqual 3455049611 <| hashFn <| toString { name = "Robin", age = "27", male = True }
+        , test "tuple" <| assertEqual 12752532 <| hashFn <| toString ( "Robin", 27, True )
+        , test "ls" <| assertEqual 4202619459 <| hashFn <| toString [1..6]
+        , test "bool" <| assertEqual 108766572 <| hashFn <| toString False
         ]
