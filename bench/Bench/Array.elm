70cf131099024639a84c7d9aee18aa9cd5231c7b
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2016-07-15 18:14:39 +0200

    Preliminary array benchmarks

--- bench/Bench/Array.elm
+++ bench/Bench/Array.elm
@@ -4,6 +4,10 @@ import Bench.Native exposing (Benchmark, BenchmarkSuite, bench, suite)
 import CollectionsNg.Array as Array exposing (Array)
 
 
+type alias Input =
+    Array Int
+
+
 tinyArraySize : Int
 tinyArraySize =
     10
@@ -24,19 +28,68 @@ largeArraySize =
     10000
 
 
-testArray : Array Int
-testArray =
-    insertBench largeArraySize ()
-
-
-insertBench : Int -> (() -> Array Int)
-insertBench n =
+build : Int -> () -> Input
+build n =
     \() -> List.foldl (\i acc -> Array.push i acc) Array.empty [1..n]
 
 
-createSuite : Int -> List (Benchmark (Array Int))
+set : Input -> () -> Input
+set arr =
+    \() -> Array.set 7 5 arr
+
+
+push : Input -> () -> Input
+push arr =
+    \() -> Array.push 5 arr
+
+
+get : Input -> () -> Maybe Int
+get arr =
+    \() -> Array.get 5 arr
+
+
+slice : Int -> Int -> Input -> () -> Input
+slice from to arr =
+    \() -> Array.slice from to arr
+
+
+map : Input -> () -> Input
+map arr =
+    \() -> Array.map ((+) 1) arr
+
+
+equality : Input -> Input -> () -> Bool
+equality a1 a2 =
+    \() -> a1 == a2
+
+
+createSuite : Int -> List Benchmark
 createSuite n =
-    [ bench "Insert" <| insertBench n ]
+    let
+        sampleArray =
+            build n ()
+    in
+        [ bench "Build"
+            <| build n
+        , bench "Set"
+            <| set sampleArray
+        , bench "Push"
+            <| push sampleArray
+        , bench "Get"
+            <| get sampleArray
+        , bench "Slice from end"
+            <| slice 0 -3 sampleArray
+        , bench "Slice from both"
+            <| slice 3 -3 sampleArray
+        , bench "Mapping"
+            <| map sampleArray
+        , bench "Equality"
+            <| equality sampleArray (Array.set 5 5 sampleArray)
+        , bench "Equality fail"
+            <| equality sampleArray (Array.set 5 7 sampleArray)
+        , bench "Equality worst case"
+            <| equality sampleArray (Array.foldl (\v acc -> Array.push v acc) Array.empty sampleArray)
+        ]
 
 
 tiny : BenchmarkSuite
