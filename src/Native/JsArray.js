5f1ab72247d0a14979b439747b796457e677ef20
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2016-10-28 03:27:34 +0200

    More compact representation of JsArray

--- src/Native/JsArray.js
+++ src/Native/JsArray.js
@@ -2,16 +2,10 @@ var _Skinney$elm_array_exploration$Native_JsArray = function() {
 /* A thin, but still immutable, wrapper over native Javascript arrays. */
 
 // An empty array
-var empty = {
-    ctor: 'JsArray',
-    _0: []
-};
+var empty = [];
 
 function singleton(val) {
-    return {
-        ctor: 'JsArray',
-        _0: [val]
-    };
+    return [val];
 }
 
 function initialize(size, offset, f) {
@@ -21,54 +15,43 @@ function initialize(size, offset, f) {
         res[i] = f(offset + i);
     }
 
-    return {
-        ctor: 'JsArray',
-        _0: res
-    };
+    return res;
 }
 
 function length(arr) {
-    return arr._0.length;
+    return arr.length;
 }
 
 function get(idx, arr) {
-    if (idx < 0 || idx >= length(arr)) {
+    if (idx < 0 || idx >= arr.length) {
         return _elm_lang$core$Maybe$Nothing;
     }
 
-    return _elm_lang$core$Maybe$Just(arr._0[idx]);
+    return _elm_lang$core$Maybe$Just(arr[idx]);
 }
 
 function set(idx, val, arr) {
-    if (idx < 0 || idx >= length(arr)) {
+    if (idx < 0 || idx >= arr.length) {
         return arr;
     }
 
-    var copy = arr._0.slice();
+    var copy = arr.slice();
     copy[idx] = val;
-
-    return {
-        ctor: 'JsArray',
-        _0: copy
-    };
+    return copy;
 }
 
 function push(val, arr) {
-    var copy = arr._0.slice();
+    var copy = arr.slice();
     copy.push(val);
-
-    return {
-        ctor: 'JsArray',
-        _0: copy
-    };
+    return copy;
 }
 
 function foldl(f, init, arr) {
     var a = init,
-        len = length(arr);
+        len = arr.length;
 
     for (var i = 0; i < len; i++) {
-        a = A2(f, arr._0[i], a);
+        a = A2(f, arr[i], a);
     }
 
     return a;
@@ -77,54 +60,45 @@ function foldl(f, init, arr) {
 function foldr(f, init, arr) {
     var a = init;
 
-    for (var i = length(arr) - 1; i >= 0; i--) {
-        a = A2(f, arr._0[i], a);
+    for (var i = arr.length - 1; i >= 0; i--) {
+        a = A2(f, arr[i], a);
     }
 
     return a;
 }
 
 function map(f, arr) {
-    var copy = arr._0.slice(),
-        len = copy.length;
+    var len = arr.length,
+        copy = new Array(len);
 
     for (var i = 0; i < len; i++) {
-        copy[i] = f(copy[i]);
+        copy[i] = f(arr[i]);
     }
 
-    return {
-        ctor: 'JsArray',
-        _0: copy
-    }
+    return copy;
 }
 
 function slice(from, to, arr) {
-    return {
-        ctor: 'JsArray',
-        _0: arr._0.slice(from, to)
-    };
+    return arr.slice(from, to);
 }
 
 function merge(dest, source, max) {
-    var destLen = dest._0.length,
+    var destLen = dest.length,
         toCopy = max - destLen,
-        sourceStop = toCopy > source._0.length ? source._0.length : toCopy,
-        combinedLen = destLen + source._0.length,
+        sourceStop = toCopy > source.length ? source.length : toCopy,
+        combinedLen = destLen + source.length,
         len = combinedLen > max ? max : combinedLen,
         arr = new Array(len);
 
     for (var i = 0; i < destLen; i++) {
-        arr[i] = dest._0[i];
+        arr[i] = dest[i];
     }
 
-    for (i = 0; i < sourceStop; i++) {
-        arr[i + destLen] = source._0[i];
+    for (var i = 0; i < sourceStop; i++) {
+        arr[i + destLen] = source[i];
     }
 
-    return {
-        ctor: 'JsArray',
-        _0: arr
-    };
+    return arr;
 }
 
 return {
