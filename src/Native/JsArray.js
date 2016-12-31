438e25f64816596e96dbec3fd1683a51a9b00459
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2016-12-31 02:35:16 +0100

    Make performance more consistent for initialize and listInitialize across browsers.

--- src/Native/JsArray.js
+++ src/Native/JsArray.js
@@ -11,7 +11,11 @@ function length(arr) {
 }
 
 function initialize(size, offset, f) {
-    var result = new Array(size);
+    if (size === 32) {
+        return initialize32(offset, f);
+    }
+
+    var result = [];
 
     for (var i = 0; i < size; i++) {
         result[i] = f(offset + i);
@@ -20,13 +24,49 @@ function initialize(size, offset, f) {
     return result;
 }
 
+function initialize32(offset, f) {
+    return [
+        f(offset),
+        f(offset + 1),
+        f(offset + 2),
+        f(offset + 3),
+        f(offset + 4),
+        f(offset + 5),
+        f(offset + 6),
+        f(offset + 7),
+        f(offset + 8),
+        f(offset + 9),
+        f(offset + 10),
+        f(offset + 11),
+        f(offset + 12),
+        f(offset + 13),
+        f(offset + 14),
+        f(offset + 15),
+        f(offset + 16),
+        f(offset + 17),
+        f(offset + 18),
+        f(offset + 19),
+        f(offset + 20),
+        f(offset + 21),
+        f(offset + 22),
+        f(offset + 23),
+        f(offset + 24),
+        f(offset + 25),
+        f(offset + 26),
+        f(offset + 27),
+        f(offset + 28),
+        f(offset + 29),
+        f(offset + 30),
+        f(offset + 31)
+    ];
+}
+
 function listInitialize(ls, max) {
-    var result = new Array(max);
+    var result = [];
     var i = 0;
 
     for (; i < max; i++) {
         if (ls.ctor === '[]') {
-            result.length = i;
             break;
         }
 
